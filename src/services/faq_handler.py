358599eead3bd3010030ca1cb3b1ab694858c4a7
what-could-possibly-go-wrong1 <whatcouldpossiblygowrong111@gmail.com>
2026-05-15 23:58:05 +0930

    fix: address PR review comments - improve FAQ matching, fix state types, update docs

--- src/services/faq_handler.py
+++ src/services/faq_handler.py
@@ -1,3 +1,5 @@
+from loguru import logger
+
 from src.services.faq_knowledge_base import FAQ_KNOWLEDGE_BASE
 
 
@@ -10,27 +12,32 @@ class FAQHandler:
         query_lower = query.lower()
         
         keyword_phrases = [
-            (["health report"], "report_info"),
+            (["health report", "my report", "view report", "check report"], "report_info"),
             (["reschedule"], "reschedule_cancel"),
-            (["cancel appointment", "cancel", "change appointment"], "reschedule_cancel"),
-            (["make an appointment", "make appointment"], "make_appointment"),
+            (["cancel appointment", "cancel my appointment", "change appointment", "want to cancel"], "reschedule_cancel"),
+            (["make an appointment", "make appointment", "i want to book"], "make_appointment"),
             (["schedule appointment", "consultation appointment"], "make_appointment"),
-            (["book appointment", "book"], "make_appointment"),
-            (["consultation cost", "cost", "free", "price", "charge"], "consultation_cost"),
-            (["consultation hours", "appointment hours", "available", "hours"], "consultation_hours"),
+            (["book appointment"], "make_appointment"),
+            (["consultation cost", "much does it cost", "cost of", "price", "charge", "is it free", "free"], "consultation_cost"),
+            (["consultation hours", "appointment hours", "opening hours", "when do you open", "when are you open", "what time do you open"], "consultation_hours"),
             (["consultation virtual", "virtual consultation", "online consultation"], "consultation_method"),
-            (["consultation location"], "consultation_location"),
-            (["where", "location"], "consultation_location"),
+            (["consultation location", "where are you located", "clinic location", "where is the consultation", "consultation location"], "consultation_location"),
             (["consultation platform", "platform", "google meet", "zoom"], "consultation_platform"),
-            (["consultation duration", "how long", "session length", "hour"], "consultation_duration"),
+            (["consultation duration", "how long is", "session length"], "consultation_duration"),
             (["data secure", "privacy", "data safe", "secure"], "data_security"),
-            (["report"], "report_info"),
         ]
         
+        matches = []
         for phrases, key in keyword_phrases:
             for phrase in phrases:
                 if phrase in query_lower:
-                    return self.knowledge_base.get(key)
+                    score = len(phrase)
+                    matches.append((score, key))
+        
+        if matches:
+            best_match = max(matches, key=lambda x: x[0])
+            logger.debug("FAQ matched: key={}, score={}", best_match[1], best_match[0])
+            return self.knowledge_base.get(best_match[1])
         
         return None
 
