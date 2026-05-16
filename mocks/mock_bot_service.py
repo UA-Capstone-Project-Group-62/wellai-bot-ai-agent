2abeafa8b8497d7ce05777686988afa1b9babe2c
Dark1nside <ashen.sb5@gmail.com>
2026-05-16 23:20:18 +0930

    revert: use original mock_bot_service without proto path change

--- mocks/mock_bot_service.py
+++ mocks/mock_bot_service.py
@@ -1,8 +1,4 @@
 from concurrent import futures
-import os
-import sys
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'proto', 'gen', 'py'))
 
 import grpc
 from proto.bot import bot_pb2, bot_pb2_grpc
